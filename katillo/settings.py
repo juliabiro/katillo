9a34bb5fef5e6bd84a39c765d85a6910077f71b7
Julia Biro <julia.biro@gmail.com>
2020-03-28 21:35:29 +0100

    heroku deploy

--- katillo/settings.py
+++ katillo/settings.py
@@ -1,3 +1,5 @@
+import django_heroku
+
 """
 Django settings for katillo project.
 
@@ -120,3 +122,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# activate django-heroku
+django_heroku.settings(locals())
