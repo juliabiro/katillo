ae2ee2b4b4cb8dc69a35248db4a44535b175d2aa
Julia Biro <julia.biro@gmail.com>
2020-03-28 18:59:44 +0100

    force redirect

--- book/views.py
+++ book/views.py
@@ -1,38 +1,59 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Olaj, Recept, Forgalmazo, Forgalmazas, Hozzavalo
 
 # Create your views here.
 
 def olaj(request, pk):
-    olaj = Olaj.objects.get(pk=pk)
-    receptek = Recept.objects.filter(hozzavalok__olaj__id=pk)
-    forgalmazok = Forgalmazas.objects.filter(olaj__id=pk) 
-    return render(request, 'book/olaj.html', {'olaj': olaj,
-                                              'receptek': receptek,
-                                              'forgalmazok': forgalmazok})
+    if request.user.is_authenticated:
+        olaj = Olaj.objects.get(pk=pk)
+        receptek = Recept.objects.filter(hozzavalok__olaj__id=pk)
+        forgalmazok = Forgalmazas.objects.filter(olaj__id=pk) 
+        return render(request, 'book/olaj.html', {'olaj': olaj,
+                                                'receptek': receptek,
+                                                'forgalmazok': forgalmazok})
+    else:
+        return redirect('/admin/login')
 
 def recept(request, pk):
-    recept = Recept.objects.get(pk=pk)
-    hozzavalok = recept.hozzavalok.all()
-    hatasok = recept.hatasok.all()
-    return render(request, 'book/recept.html', {'recept': recept, 'hatasok': hatasok, 'hozzavalok':hozzavalok})
+    if request.user.is_authenticated:
+        recept = Recept.objects.get(pk=pk)
+        hozzavalok = recept.hozzavalok.all()
+        hatasok = recept.hatasok.all()
+        return render(request, 'book/recept.html', {'recept': recept, 'hatasok': hatasok, 'hozzavalok':hozzavalok})
+    else:
+        return redirect('/admin/login')
 
 def forgalmazo(request, pk):
-    forgalmazo = Forgalmazo.objects.get(pk=pk)
-    termekek = Forgalmazas.objects.filter(ceg__id=pk)
-    return render(request, 'book/forgalmazo.html', {'forgalmazo':forgalmazo, 'termekek': termekek})
+    if request.user.is_authenticated:
+        forgalmazo = Forgalmazo.objects.get(pk=pk)
+        termekek = Forgalmazas.objects.filter(ceg__id=pk)
+        return render(request, 'book/forgalmazo.html', {'forgalmazo':forgalmazo, 'termekek': termekek})
+    else:
+        return redirect('/admin/login')
 
 def olaj_list(request):
-    olaj_list = Olaj.objects.all()
-    return render(request, 'book/olaj_list.html', {'data': olaj_list})
+    if request.user.is_authenticated:
+        olaj_list = Olaj.objects.all()
+        return render(request, 'book/olaj_list.html', {'data': olaj_list})
+    else:
+        return redirect('/admin/login')
 
 def recept_list(request):
-    recept_list = Recept.objects.all()
-    return render(request, 'book/recept_list.html', {'data': recept_list})
+    if request.user.is_authenticated:
+        recept_list = Recept.objects.all()
+        return render(request, 'book/recept_list.html', {'data': recept_list})
+    else:
+        return redirect('/admin/login')
 
 def arak_list(request):
-    arak_list = Forgalmazas.objects.all()
-    return render(request, 'book/arak_list.html', {'data': arak_list})
+    if request.user.is_authenticated:
+        arak_list = Forgalmazas.objects.all()
+        return render(request, 'book/arak_list.html', {'data': arak_list})
+    else:
+        return redirect('/admin/login')
 
 def index(request):
-    return render(request, 'book/index.html', {})
+    if request.user.is_authenticated:
+        return render(request, 'book/index.html', {})
+    else:
+        return redirect('/admin/login')
