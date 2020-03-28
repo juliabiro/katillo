686f5cfe449dab1d8a22a4e206f9bed897dc3b3b
Julia Biro <julia.biro@gmail.com>
2020-03-28 17:53:36 +0100

    alapveto nezetek es keresesek

---
+++ book/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from book import views
+
+urlpatterns = [
+    path('olajok', views.olaj_list, name='olaj_list'),
+    path('receptek', views.recept_list, name='recept_list'),
+    path('arak', views.arak_list, name='arak_list'),
+    path('olajok/<int:pk>', views.olaj, name='olaj'),
+    path('receptek/<int:pk>', views.recept, name='recept'),
+]
--- book/views.py
+++ book/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from .models import Olaj, Recept, Forgalmazo, Forgalmazas, Hozzavalo
 
 # Create your views here.
+
+def olaj(request, pk):
+    olaj = Olaj.objects.get(pk=pk)
+    receptek = Recept.objects.filter(hozzavalok__olaj__id=pk)
+    forgalmazok = Forgalmazas.objects.filter(olaj__id=pk)
+    return render(request, 'book/olaj.html', {'olaj': olaj,
+                                              'receptek': receptek})
+
+def recept(request, pk):
+    recept = Recept.objects.get(pk=pk)
+    hozzavalok = recept.hozzavalok.all()
+    hatasok = recept.hatasok.all()
+    return render(request, 'book/recept.html', {'recept': recept, 'hatasok': hatasok, 'hozzavalok':hozzavalok})
+
+def forgalmazo(request):
+    return render(request, 'book/forgalmazo.html', {})
+
+def olaj_list(request):
+    olaj_list = Olaj.objects.all()
+    return render(request, 'book/olaj_list.html', {'data': olaj_list})
+
+def recept_list(request):
+    recept_list = Recept.objects.all()
+    return render(request, 'book/recept_list.html', {'data': recept_list})
+
+def arak_list(request):
+    arak_list = Forgalmazas.objects.all()
+    return render(request, 'book/arak_list.html', {'date': arak_list})
